a53dfaba1f0e7c4fe7d18e6cbc90ec0e9f9f5152
JohanUnal29 <johan.ardilah@gmail.com>
2023-05-27 22:11:23 -0500

    intento

--- src/public/js/products-socket.js
+++ src/public/js/products-socket.js
@@ -1,108 +1,60 @@
 const socket = io();
-import ProductManager from "../managers/productManager.js";
 
-// Manager y router
-const productManager = new ProductManager();
+const formProducts = document.getElementById("mi-formulario");
+    const inputTitle = document.getElementById("name").value;
+    const inputDescript = document.getElementById("description").value;
+    const inputPrice = document.getElementById("price").value;
+    const inputThumbnails = document.getElementById("thumbnail").value;
+    const inputCode = document.getElementById("code").value;
+    const inputStock = document.getElementById("stock").value;
 
-let nombre = "";
+socket.on("products", (products) => {
+  // Como lo habia solucionado pt.1
+  // renderProducts(products);
 
-async function asyncWraper() {
-    const { value: nombreIngresado } = await Swal.fire({
-        title: "Ingresa tu nombre",
-        input: "text",
-        inputLabel: "Tu nombre",
-        inputValue: "",
-        showCancelButton: false,
-        allowOutsideClick: false,
-        inputValidator: (value) => {
-            if (!value) {
-                return "Por favor completar";
-            }
-        },
-    });
-    nombre = nombreIngresado;
-    document.getElementById("span-nombre").innerHTML = nombre;
-}
-//
-asyncWraper();
-
-
-function enviarFormulario(event) {
-    event.preventDefault(); // Evita el envío del formulario por defecto
-
-    // Obtiene los valores de los campos del formulario
-    const formulario = document.getElementById("mi-formulario");
-    const formData = new FormData(formulario);
-    const name = formData.get("name");
-    const description = formData.get("description");
-    const price = formData.get("price");
-    const thumbnail = formData.get("thumbnail");
-    const code = formData.get("code");
-    const stock = formData.get("stock");
-
-    // Crea un objeto 'product' con los datos del formulario
-    const product = {
-        name: name,
-        description: description,
-        price: price,
-        thumbnail: thumbnail,
-        code: code,
-        stock: stock
-    };
-
-    // Llama a la función 'createProduct' y pasa el objeto 'product'
-    productManager.createProduct(product);
-
-    socket.emit("msg_front_to_back", {
-        msg: product,
-    });
-}
-
-// try {
-//     const products = await productManager.getProducts(); // No está claro de dónde proviene la variable "limit". Asegúrate de definirla antes de usarla.
-
-
-// } catch (error) {
-//     console.error(`Error al obtener los productos: ${error}`);
-//     return res.status(500).json({ error: "Error al obtener los productos" });
-// }
-
-
-// const productBox = document.getElementById("input-msg");
-
-// productBox.addEventListener("keyup", ({ key }) => {
-//     //alert("toco " + key);
-//     if (key == "Enter") {
-//         socket.emit("msg_front_to_back", {
-//             msg: productBox.value,
-//             user: nombre,
-//         });
-//         productBox.value = "";
-//     }
-// });
-
-socket.on("todos_los_msgs", async (msgs) => {
-    const divMsgs = document.getElementById("div-msgs");
-    let contenido = "";
-    const products2 = await productManager.getProdycts();
-    msgs.forEach((msg) => {
-        const product = msg.msg; // Obtener el objeto 'product' del mensaje
-
-        // Construir el contenido HTML con todas las variables del producto
-        contenido += `
-            <p>Nombre: ${product.name}</p>
-            <p>Descripción: ${product.description}</p>
-            <p>Precio: ${product.price}</p>
-            <p>Thumbnail: ${product.thumbnail}</p>
-            <p>Código: ${product.code}</p>
-            <p>Stock: ${product.stock}</p>
-        `;
-    });
-
-    divMsgs.innerHTML = contenido;
-    divMsgs.innerHTML = products2;
-    
+  // Como se solicitó segun lo entendido en en el after
+  const productList = document.querySelector(".productListUpdated");
+  productList.innerHTML = `
+    ${products
+      .map(
+        (product) => `
+      <tr>
+        <th scope="row">${product.id}</th>
+        <td>${product.title}</td>
+        <td>${product.description}</td>
+        <td>${product.price}</td>
+        <td>${product.code}</td>
+        <td>${product.stock}</td>
+        <td>${product.thumbnail}</td>
+                
+        <td><button type="button" class="btn btn-danger " onclick="deleteProduct(${product.id})">X</button></td>
+      </tr>
+    `
+      )
+      .join("")}
+  `;
 });
+
+formProducts.onsubmit = (e) => {
+  e.preventDefault();
+  const newProduct = {
+    title: inputTitle.value,
+    description: inputDescript.value,
+    price: +inputPrice.value,
+    thumbnail: inputThumbnails.value,
+    code: inputCode.value,
+    stock: +inputStock.value,
+  };
+  socket.emit("new-product", newProduct);
+  formProducts.reset();
+};
+
+deleteProduct = (productId) => {
+  socket.emit("delete-product", productId);
+};
+
+
+
 
 
 
--- src/routes/products-socket.view.router.js
+++ src/routes/products-socket.view.router.js
@@ -1,8 +1,20 @@
-import express from "express";
+import { Router } from "express";
+import ProductManager from "../managers/productManager.js";
 
-export const routerViewProductsSocket = express.Router();
+// Manager y router
+const productManager = new ProductManager();
+export const routerViewProductsSocket = Router();
 
-routerViewProductsSocket.get("/", (req, res) => {
-  return res.render("realTimeProducts", {});
+routerViewProductsSocket.get("/", async (req, res) => {
+  try {
+    const products = await productManager.getProducts(); // No está claro de dónde proviene la variable "limit". Asegúrate de definirla antes de usarla.
+
+    return res.render("realTimeProducts", {
+      titulo: "TITULO: PRODUCTOS",
+      products: products,
+    });
+  } catch (error) {
+    console.error(`Error al obtener los productos: ${error}`);
+    return res.status(500).json({ error: "Error al obtener los productos" });
+  }
 });
-//
